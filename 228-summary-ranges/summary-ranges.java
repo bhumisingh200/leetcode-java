1521d4540254e7f817c5e3e9157dcb661bf030fc
bhumisingh200 <bhumi_2400526@gniot.net.in>
2026-07-05 22:29:38 +0530

    Time: 3 ms (79.26%) | Memory: 42.8 MB (84.98%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> ans=new ArrayList<>();
+        int n=nums.length;
+
+        for(int i=0; i<n; i++){
+            int start=nums[i];
+            while(i < n-1 && nums[i+1]==nums[i]+1){
+                i++;
+            }
+            if(start == nums[i]){
+                ans.add(String.valueOf(start));
+            }else{
+                ans.add(start+"->"+nums[i]);
+            }
+        }
+        return ans;
+        
+    }
+}
