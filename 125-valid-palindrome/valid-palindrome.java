c8827b0d781eea9acb62275e51883aa1b33ef456
bhumisingh200 <bhumi_2400526@gniot.net.in>
2026-07-06 14:14:20 +0530

    Time: 2 ms (99.27%) | Memory: 44.3 MB (66.00%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -4,14 +4,17 @@ class Solution {
         int i = 0;
         int j = s.length() - 1;
 
+       //loop works until i smaller then j index
         while (i < j) {
-
+            //checking if i is a ASCII character
             if (!Character.isLetterOrDigit(s.charAt(i))) {
                 i++;
             }
+            //checking if j is a ASCII character
             else if (!Character.isLetterOrDigit(s.charAt(j))) {
                 j--;
             }
+            //if index i and j character equal or not by converting it first into lower case
             else {
 
                 if (Character.toLowerCase(s.charAt(i))
