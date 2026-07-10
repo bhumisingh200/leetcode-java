7dabbabdf5553145e935a9b4924c9f6e4e9b12ba
bhumisingh200 <bhumi_2400526@gniot.net.in>
2026-07-11 01:58:50 +0530

    Time: 4 ms (82.27%) | Memory: 44.1 MB (89.70%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,4 +1,28 @@
-class Solution {
+class Solution{
+    public boolean isAnagram(String s, String t){
+
+        if(s.length()!=t.length()){
+            return false;
+        }
+
+        int[] freq=new int[26];
+
+        //Count character from s
+        for(int i=0; i<s.length(); i++){
+            freq[s.charAt(i)-'a']++;
+        }
+        //Remove Characters from t
+        for(int i=0; i<t.length();i++){
+            freq[t.charAt(i)-'a']--;
+
+            if(freq[t.charAt(i)-'a']<0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+/*class Solution {
     public boolean isAnagram(String s, String t) {
 
         if (s.length() != t.length()) {
@@ -8,27 +32,30 @@ class Solution {
         HashMap<Character, Integer> map = new HashMap<>();
 
         // Store frequency of s
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
+        for(int i=0 ; i<s.length(); i++){
+            char ch=s.charAt(i);
 
-            if (map.containsKey(ch)) {
-                map.put(ch, map.get(ch) + 1);
-            } else {
-                map.put(ch, 1);
-            }
+            if(map.containsKey(ch)){
+                map.put(ch, map.get(ch)-1);
+            }else{
+                map.put(ch,1);            }
         }
+        
 
         // Traverse t
-        for (int i = 0; i < t.length(); i++) {
-            char ch = t.charAt(i);
+        for(int i=0; i<t.length(); i++){
+            char ch=t.charAt(i);
 
-            if (!map.containsKey(ch) || map.get(ch) == 0) {
+            if(!map.containsKey(ch)||map.get(ch)==0){
                 return false;
             }
-
-            map.put(ch, map.get(ch) - 1);
         }
-
         return true;
     }
-}
+}*/
+
+//If unicode then use HASHMAP (but will be used in most)
+//If not then use ARRAY(Best Option)but limited only to some q use
+
+/*Why did you use HashMap instead of an array?
+Because HashMap works for any character set, whereas a frequency array of size 26 only works when the input is restricted to lowercase English letters.*/
