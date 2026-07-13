00c2b978e98d8e630e828554472258cd7622b338
bhumisingh200 <bhumi_2400526@gniot.net.in>
2026-07-13 10:36:01 +0530

    Time: 8 ms (75.02%) | Memory: 44 MB (26.47%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,6 +1,28 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
 
+        if (s.length() != t.length())
+            return false;
+
+        int[] mapS = new int[256];
+        int[] mapT = new int[256];
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (mapS[s.charAt(i)] != mapT[t.charAt(i)]) {
+                return false;
+            }
+
+            mapS[s.charAt(i)] = i + 1;
+            mapT[t.charAt(i)] = i + 1;
+        }
+
+        return true;
+    }
+}
+/*class Solution {
+    public boolean isIsomorphic(String s, String t) {
+
         if (s.length() != t.length()) {
             return false;
         }
@@ -34,4 +56,4 @@ class Solution {
 
         return true;
     }
-}
+}*/
