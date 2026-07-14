d554dd36daf00871d2a43ba2d879e11134a4022e
bhumisingh200 <bhumi_2400526@gniot.net.in>
2026-07-14 11:19:36 +0530

    Time: 1 ms (99.97%) | Memory: 45.8 MB (98.98%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,6 +1,30 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
 
+        int[] freq = new int[26];
+
+        // Count frequency of magazine
+        for (int i = 0; i < magazine.length(); i++) {
+            freq[magazine.charAt(i) - 'a']++;
+        }
+
+        // Use characters for ransomNote
+        for (int i = 0; i < ransomNote.length(); i++) {
+            int index = ransomNote.charAt(i) - 'a';
+
+            if (freq[index] == 0) {
+                return false;
+            }
+
+            freq[index]--;
+        }
+
+        return true;
+    }
+}
+/*class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+
         HashMap<Character, Integer> count = new HashMap<>();
 
         // Count frequency of magazine characters
@@ -24,4 +48,4 @@ class Solution {
 
         return true;
     }
-}
+}*/
