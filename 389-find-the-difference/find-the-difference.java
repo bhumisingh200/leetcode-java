44408ae847c427ff87680b3ef7d5f45443f4d6b8
bhumisingh200 <bhumi_2400526@gniot.net.in>
2026-07-14 23:33:30 +0530

    Time: 1 ms (99.87%) | Memory: 43.3 MB (35.87%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,4 +1,20 @@
-/*class Solution {
+class Solution {
+    public char findTheDifference(String s, String t) {
+        
+        char ans=0;
+
+        for(char ch : s.toCharArray()){
+            ans^=ch;
+        }
+
+        for(char ch : t.toCharArray()){
+            ans^=ch;
+        }
+        return ans;
+    }
+}
+/*Array
+class Solution {
     public char findTheDifference(String s, String t) {
 
         int[] freq = new int[26];
@@ -20,18 +36,27 @@
         return ' ';
     }
 }*/
+/*HashMap
 class Solution {
     public char findTheDifference(String s, String t) {
-        
-        char ans=0;
 
-        for(char ch : s.toCharArray()){
-            ans^=ch;
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
 
-        for(char ch : t.toCharArray()){
-            ans^=ch;
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+
+            if (!map.containsKey(ch) || map.get(ch) == 0) {
+                return ch;
+            }
+
+            map.put(ch, map.get(ch) - 1);
         }
-        return ans;
+
+        return ' ';
     }
-}
+}aq*/
