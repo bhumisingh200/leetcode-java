2ce475ec0bbc0809a3acd2bc3193ddc53ff8d653
bhumisingh200 <bhumi_2400526@gniot.net.in>
2026-07-14 23:16:18 +0530

    Time: 1 ms (99.87%) | Memory: 43.1 MB (71.70%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,3 +1,25 @@
+/*class Solution {
+    public char findTheDifference(String s, String t) {
+
+        int[] freq = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            int index = t.charAt(i) - 'a';
+
+            if (freq[index] == 0) {
+                return t.charAt(i);
+            }
+
+            freq[index]--;
+        }
+
+        return ' ';
+    }
+}*/
 class Solution {
     public char findTheDifference(String s, String t) {
         
