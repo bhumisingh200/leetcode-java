1b5ab5d626b0102ecb183d889fee98a7ca9ec9fd
bhumisingh200 <bhumi_2400526@gniot.net.in>
2026-07-15 21:57:42 +0530

    Time: 2 ms (92.46%) | Memory: 42.9 MB (85.45%) - LeetSync

--- 228-summary-ranges/summary-ranges.java
+++ 228-summary-ranges/summary-ranges.java
@@ -5,10 +5,10 @@ class Solution {
 
         for(int i=0; i<n; i++){
             int start=nums[i];
-            while(i < n-1 && nums[i+1]==nums[i]+1){
+            while(i<n-1 && nums[i+1]==nums[i]+1){   // length count 5 but index exist 4 only && next index==current index+1 
                 i++;
             }
-            if(start == nums[i]){
+            if(start==nums[i]){
                 ans.add(String.valueOf(start));
             }else{
                 ans.add(start+"->"+nums[i]);
