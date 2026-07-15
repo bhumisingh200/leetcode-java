9a8c252d831815647d02632cf9cab3e4a374f4c0
bhumisingh200 <bhumi_2400526@gniot.net.in>
2026-07-15 19:41:26 +0530

    Time: 2 ms (99.28%) | Memory: 44.1 MB (84.54%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,32 +1,28 @@
 class Solution {
     public boolean isPalindrome(String s) {
 
-        int i = 0;
-        int j = s.length() - 1;
+        int i=0;
+        int j=s.length()-1;
 
        //loop works until i smaller then j index
-        while (i < j) {
+        while(i<j){
             //checking if i is a ASCII character
-            if (!Character.isLetterOrDigit(s.charAt(i))) {
+            if(!Character.isLetterOrDigit(s.charAt(i))){
                 i++;
             }
             //checking if j is a ASCII character
-            else if (!Character.isLetterOrDigit(s.charAt(j))) {
+            else if(!Character.isLetterOrDigit(s.charAt(j))){
                 j--;
             }
             //if index i and j character equal or not by converting it first into lower case
-            else {
-
-                if (Character.toLowerCase(s.charAt(i))
-                        != Character.toLowerCase(s.charAt(j))) {
+            else{
+                if(Character.toLowerCase(s.charAt(i))!=Character.toLowerCase(s.charAt(j))){
                     return false;
                 }
-
                 i++;
                 j--;
             }
         }
-
         return true;
     }
 }
