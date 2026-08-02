02a13ca49a4536ff5d0277999186c38e1b3a16bd
bhumisingh200 <bhumi_2400526@gniot.net.in>
2026-08-02 16:15:20 +0530

    Time: 5 ms (98.56%) | Memory: 43.6 MB (87.18%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,60 +1,66 @@
 /*Array*/
-class Solution {
-    public boolean isIsomorphic(String s, String t) {
+class Solution{
+    public boolean isIsomorphic(String s, String t){
 
+        //Check if the length is equal of both the string
         if(s.length()!=t.length()){
             return false;
         }
 
+        ////Array must be writen outside the loop as it will be recreated at each looping and previous info will be erased
         int[] mapS=new int[256];
         int[] mapT=new int[256];
 
         for(int i=0; i<s.length(); i++){
-            if(mapS[s.charAt(i)]!=mapT[t.charAt(i)]){
+
+            char ch1=s.charAt(i);
+            char ch2=t.charAt(i);
+
+            if(mapS[ch1]!=mapT[ch2]){
                 return false;
             }
-            mapS[s.charAt(i)]=i+1;
-            mapT[t.charAt(i)]=i+1;
+
+            mapS[ch1]=i+1;             //here the comparison is done by increasing and comparing there index
+            mapT[ch2]=i+1;
         }
         return true;
     }
 }
 
-/*class Solution {
-    public boolean isIsomorphic(String s, String t) {
+//HashMap
+/*class Solution{
+    public boolean isIsomorphic(String s, String t){
 
-        if (s.length() != t.length()) {
+        //Check if the length is equal of both the string
+        if(s.length()!=t.length()){
             return false;
         }
 
-        HashMap<Character, Character> mapST = new HashMap<>();
-        HashMap<Character, Character> mapTS = new HashMap<>();
+        HashMap<Character,Character> MapST=new HashMap<>();    //HashMap must be writen outside the loop as it will be recreated at each looping and previous info will be erased
+        HashMap<Character,Character> MapTS=new HashMap<>();
 
-        for (int i = 0; i < s.length(); i++) {
+        for(int i=0; i<s.length(); i++){
 
-            char ch1 = s.charAt(i);
-            char ch2 = t.charAt(i);
+            char ch1=s.charAt(i);
+            char ch2=t.charAt(i);
 
-            // Check s -> t
-            if(mapST.containsKey(ch1)){
-               if(mapST.get(ch1)!=ch2){
-                 return false;
+            //check if mapS to T
+            if(MapST.containsKey(ch1)){
+                if(MapST.get(ch1)!=ch2){   //get is used to call the value of ch1
+                    return false;
                 }
             }else{
-               mapST.put(ch1,ch2);
+                MapST.put(ch1,ch2);
             }
-
-            // Check t -> s
-            if(mapTS.containsKey(ch1)){
-               if(mapTS.get(ch2)!=ch1){
-                  return false;
+            //check if mapT to S
+            if(MapTS.containsKey(ch2)){
+                if(MapTS.get(ch2)!=ch1){   //get is used to call the value of ch2
+                    return false;
                 }
             }else{
-               mapTS.put(ch2,ch1);
+                MapST.put(ch2,ch1);
             }
-            
         }
-
         return true;
     }
 }*/
