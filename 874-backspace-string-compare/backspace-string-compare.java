fb0428c817ce0ce53ff9fe140928de8e84469544
bhumisingh200 <bhumi_2400526@gniot.net.in>
2026-08-18 01:17:03 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (98.84%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -0,0 +1,55 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+
+        int i=s.length()-1;
+        int j=t.length()-1;
+
+        int skipS=0;
+        int skipT=0;
+
+        while( i>=0 || j>=0){
+
+            //Process s
+            while(i>=0){
+                if(s.charAt(i)=='#'){
+                    skipS++;
+                    i--;
+                }else if(skipS>0){
+                    skipS--;
+                    i--;
+                }else{
+                    break;
+                }
+            }
+
+            //Process t
+            while(j>=0){
+                if(t.charAt(j)=='#'){
+                    skipT++;
+                    j--;
+                }else if(skipT>0){
+                    skipT--;
+                    j--;
+                }else{
+                    break;
+                }
+            }
+
+            //compare atual character
+            if( i>=0 && j>=0){
+                if(s.charAt(i)!=t.charAt(j)){
+                    return false;
+                }
+            }
+
+            //If string are unequal
+            if((i>=0)!=(j>=0)){
+                return false;
+            }
+
+            i--;
+            j--;
+        }
+        return true;
+    }
+}
