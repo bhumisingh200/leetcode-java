c9ba18dea3fa8b9cf51398e2025b458562f7511d
bhumisingh200 <bhumi_2400526@gniot.net.in>
2026-08-19 08:29:41 +0530

    Time: 2 ms (99.33%) | Memory: 47.3 MB (23.89%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,6 +1,26 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
 
+        //initialization of hashmap
+        HashMap<Integer,Integer> ans=new HashMap<>();
+        int n=nums.length;
+
+        for(int i=0; i<n; i++){
+            int complement=target-nums[i];
+
+            if(ans.containsKey(complement)){                 //if complement already exist return (index1;index2)
+                return new int[]{ans.get(complement),i};
+            }else{
+                ans.put(nums[i],i);                          //or else put current value with there index in hashmap
+            }
+        }
+        return new int[]{};
+    }
+}
+
+/*class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
         HashMap<Integer, Integer> ans = new HashMap<>();
 
         int i = 0;
@@ -11,13 +31,13 @@ class Solution {
 
             if (ans.containsKey(complement)) {
                 return new int[]{ans.get(complement), i};
+            }else{
+                ans.put(num, i);
             }
-
-            ans.put(num, i);
 
             i++;
         }
 
         return new int[]{};
     }
-}
+}*/
