ab1fec44d2889e96c46a7dfd1bac0cf9173d64a3
bhumisingh200 <bhumi_2400526@gniot.net.in>
2026-08-19 20:35:18 +0530

    Time: 33 ms (72.38%) | Memory: 59.1 MB (60.26%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -7,7 +7,7 @@ class Solution {
         List<List<Integer>> result=new ArrayList<>();
 
         //2.Fix one element
-        for(int i=0; i<nums.length-2; i++){  //why -2? bcs When i is fixed, we need at least 2 elements after i for left and right
+        for(int i=0; i<nums.length-2; i++){//why -2? bcs When i is fixed, we need at least 2 elements after i for left and right
 
             //Skip duplicate fixed number
             if(i>0 && nums[i]==nums[i-1]){
@@ -19,18 +19,16 @@ class Solution {
             int right=nums.length-1;
 
             while(left<right){
-                
-                int sum = nums[i] + nums[left] + nums[right];
+
+                int sum=nums[i]+nums[left]+nums[right];
 
                 //Found a triple
-                if(sum==0){
-
+                if(sum==0){ 
                     result.add(Arrays.asList(
                         nums[i],
                         nums[left],
                         nums[right]
                     ));
-
                     left++;
                     right--;
 
@@ -38,12 +36,10 @@ class Solution {
                     while(left<right && nums[left]==nums[left-1]){
                         left++;
                     }
-
                     //Skip duplicate right value
                     while(left<right && nums[right]==nums[right+1]){
                         right--;
                     }
-
                 }
                 //Sum is too big
                 else if(sum>0){
