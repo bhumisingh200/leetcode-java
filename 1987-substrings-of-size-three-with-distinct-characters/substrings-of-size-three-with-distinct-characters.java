c04fa36c5512adef8d73e0181b25d2735a2a4cd7
bhumisingh200 <bhumi_2400526@gniot.net.in>
2026-08-25 08:22:41 +0530

    Time: 1 ms (96.53%) | Memory: 43 MB (41.58%) - LeetSync

---
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.java
@@ -0,0 +1,45 @@
+/*class Solution {
+    public int countGoodSubstrings(String s) {
+
+        int n=s.length();
+        int unique=0;
+
+        //First Window
+        if( s.charAt(0)!=s.charAt(1) &&
+            s.charAt(0)!=s.charAt(2) && 
+            s.charAt(1)!=s.charAt(2) ){
+            unique++;
+        }
+
+        //slide the window
+        for(int i=3; i<n; i++){
+            if(s.charAt(i-2)!=s.charAt(i-1) &&
+               s.charAt(i-2)!=s.charAt(i) && 
+               s.charAt(i-1)!=s.charAt(i) ){
+                unique++;
+            }
+        }
+        return unique;
+    }
+}*/
+
+//Different Method
+class Solution {
+    public int countGoodSubstrings(String s) {
+
+        int n = s.length();
+        int unique = 0;
+
+        for (int i = 0; i <= n - 3; i++) {
+
+            if (s.charAt(i) != s.charAt(i + 1) &&
+                s.charAt(i) != s.charAt(i + 2) &&
+                s.charAt(i + 1) != s.charAt(i + 2)) {
+
+                unique++;
+            }
+        }
+
+        return unique;
+    }
+}
