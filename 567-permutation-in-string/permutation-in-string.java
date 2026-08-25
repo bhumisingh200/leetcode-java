286de61d23d456a1133d8acaeaa9007973102ff7
bhumisingh200 <bhumi_2400526@gniot.net.in>
2026-08-25 22:18:00 +0530

    Time: 32 ms (19.63%) | Memory: 46.9 MB (17.75%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -16,15 +16,17 @@ class Solution {
 
         //map1 → frequency of s1
         for(int i = 0; i < k; i++) {                       
-            char ch = s1.charAt(i); 
-            map1.put(ch, map1.getOrDefault(ch, 0) + 1); 
+            char ch1 = s1.charAt(i); 
+            map1.put(ch1, map1.getOrDefault(ch1, 0) + 1); 
+            char ch2 = s2.charAt(i);
+            map2.put(ch2, map2.getOrDefault(ch2, 0) + 1);
         }
 
         //map2 → frequency of first window of s2
-        for(int i = 0; i < k; i++) {                       
+        /*for(int i = 0; i < k; i++) {                       
             char ch = s2.charAt(i);
             map2.put(ch, map2.getOrDefault(ch, 0) + 1);
-        }
+        }*/
 
         //Check first window
         if(map1.equals(map2)){
