85fa91484a265156900fac4671b9b4b50d64f87c
bhumisingh200 <bhumi_2400526@gniot.net.in>
2026-08-27 20:16:46 +0530

    Time: 24 ms (28.33%) | Memory: 46.9 MB (14.44%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,61 +1,57 @@
-class Solution {
-    public boolean checkInclusion(String s1, String s2) {
+class Solution{
+    public boolean checkInclusion(String s1, String s2){
 
-        int left = 0;
-        int k = s1.length();
-        int right = k;
-        int n = s2.length();
+        int left=0;
+        int k=s1.length();
+        int right=k;
+        int n=s2.length();
 
-        //Edge Case
-        if(k > n){
+        //Edge case
+        if(k>n){
             return false;
         }
 
-        HashMap<Character,Integer> map1=new HashMap<>();    //frequency of characters in s1
-        HashMap<Character,Integer> map2=new HashMap<>();    //frequency of characters in current window of s2
+        HashMap<Character,Integer> map1=new HashMap<>();
+        HashMap<Character,Integer> map2=new HashMap<>();
 
-        //map1 → frequency of s1
-        for(int i = 0; i < k; i++) {                       
-            char ch = s1.charAt(i); 
-            map1.put(ch, map1.getOrDefault(ch, 0) + 1); 
+        //Enter s1 char in map1
+        for(int i=0; i<k; i++){
+            char ch=s1.charAt(i);
+            map1.put(ch,map1.getOrDefault(ch,0)+1);
+        }
+        //Enter s2 char in map2
+        for(int i=0; i<k; i++){
+            char ch=s2.charAt(i);
+            map2.put(ch,map2.getOrDefault(ch,0)+1);
         }
 
-        //map2 → frequency of first window of s2
-        for(int i = 0; i < k; i++) {                       
-            char ch = s2.charAt(i);
-            map2.put(ch, map2.getOrDefault(ch, 0) + 1);
-        }
-
-        //Check first window
+        //Check current window
         if(map1.equals(map2)){
             return true;
         }
 
-        //SLide the window
-        while(right<s2.length()){
-            //Removing the element
+        //Slide the s2 window
+        while(right<n){
+
+            //Remove left character
             char remove=s2.charAt(left);
-            map2.put(remove,map2.get(remove)-1);
+            map2.put(remove , map2.get(remove)-1);
+
             if(map2.get(remove)==0){
                 map2.remove(remove);
             }
             left++;
 
-            //Add character entering the window
+            //Add right character
             char add=s2.charAt(right);
-            map2.put(add,map2.getOrDefault(add,0)+1);
+            map2.put(add, map2.getOrDefault(add, 0)+1);
             right++;
 
-            //Check current window
-            if(map1.equals(map2)) {
+            //Check each slided window
+            if(map1.equals(map2)){
                 return true;
             }
-            
-            
-
         }
         return false;
     }
 }
-            
-            
