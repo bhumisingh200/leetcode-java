b1769cdf7d3c22a504bebc97c825762157e47ce6
bhumisingh200 <bhumi_2400526@gniot.net.in>
2026-08-31 10:00:43 +0530

    Time: 8 ms (70.59%) | Memory: 48 MB (11.39%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -1,19 +1,18 @@
 class NumArray {
 
     //Initializes an array called prifix outside such that it should be accessed by sumRange as well
-    int[] prifix;
+    int[] prefix;
 
     public NumArray(int[] nums) {
-        //Local initialization
-        prifix=new int[nums.length+1]; 
+        prefix=new int[nums.length+1];
 
         for(int i=0; i<nums.length; i++){
-            prifix[i+1]=prifix[i]+nums[i];
+            prefix[i+1]=prefix[i]+nums[i];
         }
     }
     
     public int sumRange(int left, int right) {
-        return prifix[right+1] - prifix[left];
+        return prefix[right+1]-prefix[left];
     }
 }
 
