00f2373eeaa59e2368f393384ff22db474cf8ace
bhumisingh200 <bhumi_2400526@gniot.net.in>
2026-09-06 15:49:07 +0530

    Time: 1396 ms (84.46%) | Memory: 0B (100.00%) - LeetSync

---
+++ 607-sales-person/sales-person.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT s.name
+FROM SalesPerson s
+WHERE NOT EXISTS(
+    SELECT 1
+    FROM Company c
+    JOIN Orders o
+    ON c.com_id=o.com_id
+    WHERE o.sales_id=s.sales_id
+    AND c.name='RED'
+);
