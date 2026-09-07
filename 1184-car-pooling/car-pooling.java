8b7be2de5cc3bc1a7ed6f24626b9df8265179f34
bhumisingh200 <bhumi_2400526@gniot.net.in>
2026-09-07 22:44:10 +0530

    Time: 1 ms (100.00%) | Memory: 46.4 MB (25.16%) - LeetSync

--- 1184-car-pooling/car-pooling.java
+++ 1184-car-pooling/car-pooling.java
@@ -9,7 +9,7 @@ class Solution {
             int from=trip[1];
             int to=trip[2];
 
-            //Add passenger at first index & subtract at last+1 index
+            //Add passenger at first index & subtract at last index
             diff[from]+=numPassengers;    //If you do:diff[from - 1] += passengers;then:from = 1,from - 1 = 0 ,passengers get on at stop 0 which is wrong.
             diff[to]-=numPassengers;
         }
