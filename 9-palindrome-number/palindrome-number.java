4c4ca7f3833c16b418c42bf6e6f23e8070a1266a
bhumisingh200 <bhumi_2400526@gniot.net.in>
2026-09-13 01:30:26 +0530

    Time: 5 ms (82.70%) | Memory: 46.1 MB (33.01%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,4 +1,6 @@
-class Solution {
+//Optimal in Asymptotic Complexity
+//Time:O(log10 x) Space:O(1)
+/*class Solution {
     public boolean isPalindrome(int x) {
 
         int original=x;
@@ -11,5 +13,25 @@ class Solution {
             x=x/10;
         }
         return(original==reverse);
+    }
+}*/
+
+//Time:O(log n) Space:O(1)
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int original = x;
+        int reverse = 0;
+
+        while (x > 0) {
+            reverse = reverse * 10 + x % 10;
+            x /= 10;
+        }
+
+        return original == reverse;
     }
 }
