dccc71c4739d76e094c9e312fdc72ff6c92312dd
bhumisingh200 <bhumi_2400526@gniot.net.in>
2026-09-13 12:09:43 +0530

    Time: 4 ms (94.90%) | Memory: 77.5 MB (12.79%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,21 +1,22 @@
 class Solution {
     public int maxArea(int[] height) {
-        
+
         int n=height.length;
         int maxArea=0;
         int i=0;
         int j=n-1;
-
+        
         while(i<j){
-            //Calculating the area
+
+            //Calculate the area
             int area=(j-i)*(Math.min(height[i],height[j]));
 
-            //Find the maxArea
+            //Check maxArea with area
             if(maxArea<area){
                 maxArea=area;
             }
 
-            //Moving pointer only if any 1 of the height is shorter
+            //Move the min pointer
             if(height[i]<height[j]){
                 i++;
             }else{
