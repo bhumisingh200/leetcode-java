01c6e20cd87ed1f39bc9324e84ea494eb830242e
bhumisingh200 <bhumi_2400526@gniot.net.in>
2026-09-23 00:57:16 +0530

    Time: 685 ms (63.55%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -5,8 +5,8 @@ SELECT
         2
     ) AS immediate_percentage
 FROM Delivery
-WHERE (customer_id, order_date) IN (
-    SELECT customer_id, MIN(order_date)
+WHERE (customer_id, order_date) IN (    #Does this exact pair exist in the result of my subquery?
+    SELECT customer_id, MIN(order_date) #Give me the first order date of every customer.
     FROM Delivery
     GROUP BY customer_id
 );
